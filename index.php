e44f7683027b226d1074a80f45c33bb697db2bc6
zulfiadi-op <adizul299@gmail.com>
2025-08-12 09:53:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 <body>
     <h1>coba git </h1>
     <p>Lorem ipsum dolor sit 
-        amet consectetur adipisicing elit. Quos dolores laborum eveniet nostrum fuga ad, earum, iusto enim laudantium porro sint sit? Porro deserunt vitae nostrum reprehenderit quos! Quia, eius!</p>
+        amet consectetur adipisicing elit. 
+        Quos dolores laborum eveniet 
+        nostrum fuga ad, earum, 
+        iusto enim laudantium porro
+         sint sit? Porro deserunt
+          vitae nostrum reprehenderit quos! Quia, eius!</p>
+    <ul>
+        <li>jamet</li>
+        <li>butut</li>
+    </ul>
 </body>
 </html>
